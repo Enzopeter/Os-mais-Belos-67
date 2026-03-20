d68bca5399582ae54ed755d8849b6d20ae6a27f3
Enzopeter <enzocpeter@gmail.com>
2026-03-20 09:32:49 -0300

    adicionado os alunos 2 e 3, além do print de cada nome

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main() {
-    char *Nome_do_Aluno1 = "pedro";
+    char *Nome_do_Aluno1 = "Enzo";
     printf("ola %s\n", Nome_do_Aluno1);
-    
+    char *Nome_do_Aluno2 = "Leonardo";
+    printf("ola %s\n", Nome_do_Aluno2);
+    char *Nome_do_Aluno3 = "caio";
+    printf("ola %s\n", Nome_do_Aluno3);
     return 0;
 }
