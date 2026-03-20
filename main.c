40902cf95c718a19b00f50a025e40623ea61ffb4
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-20 09:07:18 -0300

    Adicionado biblioteca, int, print e arquivo

--- main.c
+++ main.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
-<<<<<<< HEAD
-int main() {
-    char *Nome_do_Aluno1 = "Enzo";
-    printf("ola %s\n", Nome_do_Aluno1);
-    char *Nome_do_Aluno2 = "Leonardo";
-    printf("ola %s\n", Nome_do_Aluno2);
-    char *Nome_do_Aluno3 = "caio";
-    printf("ola %s\n", Nome_do_Aluno3);
-    return 0;
-}
-=======
 int main(){
-printf("Por favor me da 10\n");
+printf("Quero 10\n");
 return 0;
 
 
 } 
->>>>>>> 0501d54 (Adicionado biblioteca, int, print e arquivo)
