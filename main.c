f415e57c2fa9b41e4594a8fe9e344dd7f1953390
leomaulicino <leo.maulicino@gmail.com>
2026-03-20 10:20:47 -0300

    Explicação do que foi realizado no código

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ printf("Quero 10\n");
 printf("Enzo Peter\n");
 printf("Caio\n");
 printf("Leonardo Robert\n");
+//Foram adicionado o nome dos alunos, e um print de cada nome com outro pedindo nota 10 na atividade :)
 return 0;
 
 
