25c4fb122fb3bcfab2beacf6393bec987a263d38
leomaulicino <leo.maulicino@gmail.com>
2026-03-20 09:46:03 -0300

    Problema resolvido em c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+<<<<<<< HEAD
 int main() {
     char *Nome_do_Aluno1 = "Enzo";
     printf("ola %s\n", Nome_do_Aluno1);
@@ -8,3 +9,11 @@ int main() {
     printf("ola %s\n", Nome_do_Aluno3);
     return 0;
 }
+=======
+int main(){
+printf("Por favor me da 10\n");
+return 0;
+
+
+} 
+>>>>>>> 0501d54 (Adicionado biblioteca, int, print e arquivo)
