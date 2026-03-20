afc7baa7f9918b7b6bcd3f1bd94bee4c78a49853
chferrazsilva2@gmail.com <labsfiap@fiap.com.br>
2026-03-20 10:18:28 -0300

    Adicionando o nome do aluno na linha 6

--- main.c
+++ main.c
@@ -3,6 +3,7 @@ int main.c(){
     printf("Bom dia grupo\n");
     printf("Caio Henrique Ferraz da Silva");
     printf("Leonardo integrante do grupo");
+    printf("Enzo novo integrant ");
 
 return 0;
 
